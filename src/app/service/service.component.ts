bfa8b647829d0769e58ddd9acb2f5b0842862efb
kanthakumar <kanthakumar.r@contus.in>
2025-04-06 18:51:42 +0530

    Footer remain

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./service.component.scss']
 })
 export class ServiceComponent {
+  testImages: Array<string>;
 
+  breakpoints = {
+    1024: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 20
+    },
+    767: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 15
+    },
+    480: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 10
+    }
+    ,
+    0: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 10
+    }
+  };
+
+  constructor() {
+    this.testImages = [
+      "../../assets/man.png",
+      "../../assets/man.png",
+      "../../assets/man.png",   "../../assets/man.png",
+      "../../assets/man.png",
+      "../../assets/man.png",   "../../assets/man.png",
+      "../../assets/man.png",
+      "../../assets/man.png",   "../../assets/man.png",
+      "../../assets/man.png",
+      "../../assets/man.png",   "../../assets/man.png",
+      "../../assets/man.png",
+      "../../assets/man.png",   "../../assets/man.png",
+      "../../assets/man.png",
+      "../../assets/man.png",
+    ]
+  }
 }
