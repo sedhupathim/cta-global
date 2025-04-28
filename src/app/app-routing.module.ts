61623c4fb9a9b52bb8c00619e7a9f8a19453b9eb
sedhu_1601 <sedhupathi.m@contus.in>
2025-04-29 00:00:37 +0530

    Product List page work started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductListComponent } from './product-list/product-list.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: '**', redirectTo: '' }, // Optional: redirect unknown routes to home
+];
 
 @NgModule({
   imports: [    
